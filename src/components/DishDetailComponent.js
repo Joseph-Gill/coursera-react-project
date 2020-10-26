8e1642d7724b8691c0a0b0bdfb7ae7116cb9b200
Joseph Edwin Gill <josephedwingill@gmail.com>
2020-10-26 16:56:12 +0100

    SPA pt2

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import {Card, CardBody, CardImg, CardText, CardTitle} from "reactstrap";
+import {Card, CardBody, CardImg, CardText, CardTitle, Breadcrumb, BreadcrumbItem} from "reactstrap";
+import {Link} from "react-router-dom";
 
 const RenderComments = ({comments}) => {
   if (comments) {
     return (
-      <>
-        <h3>Comments</h3>
+      <div className="col-12 col-md-5 m-1">
+        <h4>Comments</h4>
         <ul className="list-unstyled">
           {comments.map(comment => (
             <div key={comment.id} className="mb-3">
-              <li><p>{comment.comment}</p></li>
-              <li><p>{`-- ${comment.author} , ${new Intl.DateTimeFormat('en-us', {
-                year: 'numeric',
-                month: 'short',
-                day: '2-digit'
-              }).format(new Date(Date.parse(comment.date)))}`}</p></li>
+              <li><p>{comment.comment}</p>
+                <p>{`-- ${comment.author} , ${new Intl.DateTimeFormat('en-us', {
+                  year: 'numeric',
+                  month: 'short',
+                  day: '2-digit'
+                }).format(new Date(Date.parse(comment.date)))}`}</p></li>
             </div>))}
         </ul>
-      </>
+      </div>
     )
   } else {
     return <div/>
@@ -27,19 +28,14 @@ const RenderComments = ({comments}) => {
 const RenderDish = ({dish}) => {
   if (dish) {
     return (
-      <div className="row">
-        <div className="col-12 col-md-5 m-1">
-          <Card>
-            <CardImg width="100%" src={dish.image} alt={dish.name}/>
-            <CardBody>
-              <CardTitle>{dish.name}</CardTitle>
-              <CardText>{dish.description}</CardText>
-            </CardBody>
-          </Card>
-        </div>
-        <div className="col-12 col-md-5 m-1">
-          <RenderComments comments={dish.comments} />
-        </div>
+      <div className="col-12 col-md-5 m-1">
+        <Card>
+          <CardImg width="100%" src={dish.image} alt={dish.name}/>
+          <CardBody>
+            <CardTitle>{dish.name}</CardTitle>
+            <CardText>{dish.description}</CardText>
+          </CardBody>
+        </Card>
       </div>
     );
   } else {
@@ -47,10 +43,23 @@ const RenderDish = ({dish}) => {
   }
 }
 
-const DishDetail = ({dish}) => {
+const DishDetail = ({dish, comments}) => {
   return (
     <div className="container">
-      <RenderDish dish={dish} />
+      <div className="row">
+        <Breadcrumb>
+          <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+          <BreadcrumbItem active>{dish.name}</BreadcrumbItem>
+        </Breadcrumb>
+        <div className="col-12">
+          <h3>{dish.name}</h3>
+          <hr/>
+        </div>
+      </div>
+      <div className="row">
+        <RenderDish dish={dish} comments={comments}/>
+        <RenderComments comments={comments}/>
+      </div>
     </div>
   );
 }
